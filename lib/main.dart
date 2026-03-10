61e2827034274517a5806818a1a6133f9eafba38
peach-01 <cooperpeach5@gmail.com>
2026-03-10 12:12:44 -0500

    added crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'auth/auth_gate.dart';
 import './firebase_options.dart';
 
-/*void main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    // catch flutter framework errors
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    // catch async / dart errors
+    PlatformDispatcher.instance.onError = (e, stack) {
+      FirebaseCrashlytics.instance.recordError(e, stack, fatal: true);
+    }
+    
   } catch (e) {
     debugPrint("Firebase init error");
   }
   runApp(const VigilCollectorApp());
-}*/
+}
 
-void main() {
+/*void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(body: Center(child: Text("TEST SCREEN"))),
     ),
   );
-}
+}*/
 
 class VigilCollectorApp extends StatelessWidget {
   const VigilCollectorApp({super.key});
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import firebase_analytics
 import firebase_auth
 import firebase_core
+import firebase_crashlytics
 import flutter_blue_plus_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
 }
